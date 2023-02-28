978efbacc3077658f298de899d784b90122e322d
Kuldip Rupsangbhai Parmar <116505382+PKR9759@users.noreply.github.com>
2023-02-28 22:50:07 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
- int fun(int &num){
+int fun(int &num){
+	num++;
 	return num;
 }
-
 
 int main(){
 
 	int i=8;
-    const int &r=fun(i);
+	const int &r=fun(i);
+	;
+	cout<<r<<' '<<i<<endl;
 	cout<<&r<<' '<<&i;
 }
