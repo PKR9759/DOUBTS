44cd57876b4771c3c839b4b2cf6cd2a6308f7b8a
Kuldip Rupsangbhai Parmar <116505382+PKR9759@users.noreply.github.com>
2023-03-29 03:41:09 +0530

    Create fd.cpp

---
+++ fd.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int fi(int a,flaot b,int c){
+	cout<<"f1";
+}
+int fi(double a,double b,bool c){
+	cout<<"f2";
+}
+
+
+int main(){
+	fi(3,3.1,false);
+}
